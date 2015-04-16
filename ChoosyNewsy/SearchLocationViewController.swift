7476d5b12f99f4d36ac4484f6969348580b05522
Tim Gilman <timothy.gilman@yahoo.com>
2015-04-16 17:12:15 -0500

    Add Search

---
+++ ChoosyNewsy/SearchLocationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SearchLocationViewController.swift
+//  ChoosyNewsy
+//
+//  Created by Tim Gilman on 4/14/15.
+//  Copyright (c) 2015 Tim Gilman. All rights reserved.
+//
+
+import UIKit
+
+class SearchLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
