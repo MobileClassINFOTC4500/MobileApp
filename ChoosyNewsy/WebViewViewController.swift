aeb7ee82cc76af9b74aa94a38f6a457dbc3575a2
egibs <theg859@gmail.com>
2015-04-16 17:52:38 -0500

    web view controller mostly finished

---
+++ ChoosyNewsy/WebViewViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewViewController.swift
+//  ChoosyNewsy
+//
+//  Created by Evan Gibler on 4/16/15.
+//  Copyright (c) 2015 Tim Gilman. All rights reserved.
+//
+
+import UIKit
+
+class WebViewViewController: UIViewController {
+
+    var url = ""
+    
+    //missing IBOutlet (create later obviously)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //IBOutletName.loadRequest(NSURLRequest(URL: NSURL(string: url)!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
